5f3b6e3146f112333421510eb9be4f2f1453345f
Debdayal Mandal <debdayalmandal@hotmail.com>
2012-04-06 23:30:51 -0600

    commited

--- git-test/src/Main.java
+++ git-test/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my first git project");
+		System.out.println("Changed something ...");
 	}
 
 }
